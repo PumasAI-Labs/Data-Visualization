dd8bcfa22a8231d5530ec2fef110791c874607f0
Juan José González Oneto <80299581+jotas6@users.noreply.github.com>
2023-08-31 11:38:24 -0500

    Apply JuliaFormatter (#2)

--- 04-algebra.jl
+++ 04-algebra.jl
@@ -3,23 +3,21 @@
 include("03-geometries.jl")
 
 # Mean concentration on top of all concentration profiles for each dose level
-plt_mean = data(iv_summary) *
-            mapping(
-                :time,
-                :conc_mean;
-                layout=:dose => nonnumeric,
-            ) *
-            visual(ScatterLines)
+plt_mean =
+  data(iv_summary) *
+  mapping(:time, :conc_mean; layout = :dose => nonnumeric) *
+  visual(ScatterLines)
 draw(plt_mean) # So far we only have the mean concentration profiles
 
-plt_all = data(df_iv) *
-            mapping(
-                :time,
-                :conc;
-                layout=:dose => nonnumeric,
-                group=:id => nonnumeric # Required
-            ) *
-            visual(ScatterLines; color=(:grey, 0.5)) # We will talk more about customization later
+plt_all =
+  data(df_iv) *
+  mapping(
+    :time,
+    :conc;
+    layout = :dose => nonnumeric,
+    group = :id => nonnumeric, # Required
+  ) *
+  visual(ScatterLines; color = (:grey, 0.5)) # We will talk more about customization later
 draw(plt_all) # Here we only have all the concentration profiles
 
 # We can add both plots
@@ -28,16 +26,10 @@ draw(plt)
 
 # Scatter plot with a linear regression on top
 data(demographics_df) *
-    mapping(
-        :AGE,
-        :eGFR
-    ) *
-    (visual(Scatter) + AlgebraOfGraphics.linear()) |> draw # Distributive law
+mapping(:AGE, :eGFR) *
+(visual(Scatter) + AlgebraOfGraphics.linear()) |> draw # Distributive law
 
 ## We can also add a smoothing line
 data(demographics_df) *
-    mapping(
-        :AGE,
-        :eGFR
-    ) *
-    (visual(Scatter) + AlgebraOfGraphics.linear() + smooth()) |> draw # Both Pumas an AoG export linear
+mapping(:AGE, :eGFR) *
+(visual(Scatter) + AlgebraOfGraphics.linear() + smooth()) |> draw # Both Pumas an AoG export linear
