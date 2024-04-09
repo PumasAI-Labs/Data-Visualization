9bf90b0e1ddea44a6c411414be93b80b564eb1cf
Juan José González Oneto <80299581+jotas6@users.noreply.github.com>
2024-04-09 03:39:38 -0500

    Change indent to 4 (#3)
    * Change indent to 4
    
    * Format code with JuliaFormatter

--- 02-mappings.jl
+++ 02-mappings.jl
@@ -3,32 +3,32 @@ include("01-layers.jl")
 
 # Coloring
 plt_colors =
-  data(demographics_df) * mapping(
-    :AGE,
-    :WEIGHT;
-    color = :ISMALE, # We want to have different colors according to sex
-  ) * visual(Scatter)
+    data(demographics_df) * mapping(
+        :AGE,
+        :WEIGHT;
+        color = :ISMALE, # We want to have different colors according to sex
+    ) * visual(Scatter)
 draw(plt_colors) # Works, but it is interpreting :ISMALE as a continuous variable
 
 ## We can tell AoG that :ISMALE is categorical with nonnumeric
 plt_colors =
-  data(demographics_df) *
-  mapping(:AGE, :WEIGHT; color = :ISMALE => nonnumeric) *
-  visual(Scatter)
+    data(demographics_df) *
+    mapping(:AGE, :WEIGHT; color = :ISMALE => nonnumeric) *
+    visual(Scatter)
 draw(plt_colors) # Now it works as expected
 
 # Markers
 plt_markers =
-  data(demographics_df) *
-  mapping(:AGE, :WEIGHT; marker = :ISMALE => nonnumeric) *
-  visual(Scatter)
+    data(demographics_df) *
+    mapping(:AGE, :WEIGHT; marker = :ISMALE => nonnumeric) *
+    visual(Scatter)
 draw(plt_markers)
 
 ## Tip: you can combine different arguments in a single plot
 plt_combined =
-  data(demographics_df) *
-  mapping(:AGE, :WEIGHT; color = :ISMALE => nonnumeric, marker = :ISMALE => nonnumeric) *
-  visual(Scatter)
+    data(demographics_df) *
+    mapping(:AGE, :WEIGHT; color = :ISMALE => nonnumeric, marker = :ISMALE => nonnumeric) *
+    visual(Scatter)
 draw(plt_combined)
 
 ## There are many more options depending on the type of plot that you are creating
@@ -36,23 +36,23 @@ draw(plt_combined)
 # Faceting
 ## Columns
 plt_cols =
-  data(demographics_df) *
-  mapping(:AGE, :WEIGHT; col = :ISMALE => nonnumeric) *
-  visual(Scatter)
+    data(demographics_df) *
+    mapping(:AGE, :WEIGHT; col = :ISMALE => nonnumeric) *
+    visual(Scatter)
 draw(plt_cols)
 
 ## Rows
 plt_rows =
-  data(demographics_df) *
-  mapping(:AGE, :WEIGHT; row = :ISMALE => nonnumeric) *
-  visual(Scatter)
+    data(demographics_df) *
+    mapping(:AGE, :WEIGHT; row = :ISMALE => nonnumeric) *
+    visual(Scatter)
 draw(plt_rows)
 
 ## Layout
 df_iv = dataset("iv_sd_1") # New dataset
 
 plt_subjects =
-  data(df_iv) * # Concentration profiles for each subject
-  mapping(:time, :conc; layout = :id => nonnumeric) *
-  visual(Lines) # New type of plot (we will see more later)
+    data(df_iv) * # Concentration profiles for each subject
+    mapping(:time, :conc; layout = :id => nonnumeric) *
+    visual(Lines) # New type of plot (we will see more later)
 draw(plt_subjects)
